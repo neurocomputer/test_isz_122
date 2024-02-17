8bca68821d3247365c3a80a722bbc18d4f4b56ae
Sarah Smith <sarah.smith@email.com>
2024-02-17 15:20:13 +0300

    привет

---
+++ data.py
@@ -0,0 +1,16 @@
+"""
+Структуры данных в Python
+кортежи
+множества
+коллекции
+Изменяемые и неизменяемые типы данных в Python
+"""
+
+# однострочный комментарий
+a = 1 # int
+b = 2.5 # float
+c = 'hello' # string
+d = [1, 2, 5.5, "name", [1,3,"hello"]] # list
+e = {"name": "Ivan", "age": 25} # dict
+f = True # bool
+g = None # none
---
+++ functions.py
@@ -0,0 +1,7 @@
+def func_name(arg1, arg2):
+    print(arg1)
+    print(arg2)
+    return arg1 + arg2
+
+result = func_name(1, 2)
+print(result)
